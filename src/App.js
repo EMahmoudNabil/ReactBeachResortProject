9a32fab3f3fcd5f0a2e26a27b584d08f097e30ef
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-08-02 23:23:32 +0200

    structure components & router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-// import './App.css';
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import Home from './components/Home'
+import Rooms from './components/Room'
+import SingleRoom from './components/SingleRoom'
+import Error from './components/Error'
+import {  Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render(){
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+<Fragment>
+  <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms" component={Rooms} />
+      <Route exact  path="/rooms/:slug" component={SingleRoom} />
+      <Route  component={Error} />
+
+    </Switch>
+</Fragment>
+
   );
 }}
 
