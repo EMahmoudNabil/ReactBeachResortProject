98262a14f6d6fcf96483330dea3b12642fe8ce66
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-08-03 11:04:56 +0200

    Hero component

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 export default function Room() {
     return (
-        <div>
-            Hello from rooms page
-        </div>
+      <Hero hero="roomsHero " />
     )
 }
