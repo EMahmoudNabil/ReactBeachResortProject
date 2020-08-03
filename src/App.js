fd4319cd4ad64ed53d41ae3d7142a02ba7d5c2d4
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-08-03 10:43:53 +0200

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import Home from './components/Home'
-import Rooms from './components/Room'
-import SingleRoom from './components/SingleRoom'
-import Error from './components/Error'
+import Home from './pages/Home'
+import Rooms from './pages/Room'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+import Navbar from './components/Navbar'
 import {  Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render(){
   return (
 <Fragment>
+<Navbar />
   <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/rooms" component={Rooms} />
