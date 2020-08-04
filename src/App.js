1f551f1f2e675f97615aee2018cd358374c132a5
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-08-04 15:31:50 +0200

    context $ feasure component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import Home from './pages/Home'
-import Rooms from './pages/Room'
+import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 import Navbar from './components/Navbar'
