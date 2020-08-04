b13af04f3fc8530ec3860d9909939679e0c6c326
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-08-04 10:36:25 +0200

    format data function

--- src/api/context.js
+++ src/api/context.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import items from './data'
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
     state={
-        greading:"hello",
-        name:"Mahmoud"
+        rooms:[],
+        sortedRooms:[],
+        featuredRoom:[],
+        loading:true
+    }
+    // getData
+    componentDidMount(){
+        let rooms=this.formatData(items);
+        console.log(rooms)
+    }
+    formatData(items){
+        let tempItem =items.map(item =>{
+            let id = item.sys.id
+            let images =item.fields.images.map(image =>
+                image.fields.file.url)
+            let room ={...item.fields ,images,id}
+            return room;   
+        })
+        return tempItem
     }
     render() {
         return (
--- src/components/FeasureRoom.js
+++ src/components/FeasureRoom.js
@@ -4,11 +4,10 @@ import {RoomContext} from '../api/context'
 export default class FeasureRoom extends Component {
     static contextType = RoomContext
     render() {
-        const {name , greading} =this.context;
-        // console.log(value)
+
         return (
             <div>
-                {greading}, {name} from feature rooms
+                hello from feature rooms
             </div>
         )
     }
